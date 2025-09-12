f155a79cd51ab7cf245b5a7ade877fb2b84d0ca8
Zhora Tashchyan <zhoratashchyan5@gimail.com>
2025-09-13 02:26:49 +0400

    homework 2

---
+++ src/homework3/FigurePainter4.java
@@ -0,0 +1,17 @@
+package homework3;
+
+public class FigurePainter4 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j < n; j++) {
+                System.out.print("  "); // пробелы
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
